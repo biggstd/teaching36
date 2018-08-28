3e2211860811cde134f9e8550fa9b7b23c5aace3
Tyler Biggs <biggstd@gmail.com>
2018-08-28 15:42:54 -0700

    Lambda input requirement removed.

--- gillespie/numpyGillespie.py
+++ gillespie/numpyGillespie.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from collections import Counter
+from math import factorial
+
 import numpy as np
 import pandas as pd
 
 
+
+def permutation_count(species, species_delta):
+    """Calculates the number of possible number of reactions for a
+    given species and its changes.
+    """
+
+    # Get the negative species changes. These are components that
+    # must be consumed for the reaction to take place, and therefore
+    # also determine the number of possible reactions.
+    reaction_members = np.sum(np.abs(species_delta[species_delta != 0]))
+
+    choose_from = 0
+    for count, delta in zip(species, species_delta):
+        if delta != 0:
+            choose_from += count
+
+    print(choose_from - reaction_members)
+    if choose_from - reaction_members <= 0:
+        return 0
+    # print(choose_from, reaction_members)
+    return factorial(choose_from) // (factorial(reaction_members) * factorial(choose_from - reaction_members))
+
+
 class Gillespie:
 
-    def __init__(self, species, rates, species_changes, permutations,
-                 max_sim_rxn, max_sim_time=None):
+    def __init__(self, species, rates, species_changes, max_sim_rxn):
         """Assigns the input values to appropriate numpy arrays.
 
         :param species:
@@ -21,18 +46,9 @@ class Gillespie:
             Changes to the species, indexed the same as the reaction
             rate list.
 
-        :param permutations:
-            A list of functions that calculate the permutations
-            available for each of the reactions tracked. Should be
-            indexed the same as `species_change` and `rates`.
-
         :param max_sim_rxn:
             The maximum number of reactions that the program will be
             allowed to simulate.
-
-        :param max_sim_time:
-            The maximum simulation time the program will be allowed
-            to run. This time depends on the scale chosen in the rates.
 
         """
 
@@ -58,10 +74,9 @@ class Gillespie:
         self.rxn_count = 1
 
         # Assign the inputs to the class instance.
-        self.species_changes = species_changes
-        self.permutations = permutations
+        self.species_changes = np.array(species_changes, dtype=np.int)
 
-    def calc_av(self, curr_species):
+    def calc_av(self):
         """Calculate the Av value.
 
         The current permutations multiplied be the rates.
@@ -70,13 +85,15 @@ class Gillespie:
         creates a new 1-dimensional array from an iterable object.
         """
 
-        # Create an generator that is iterable from the lambda
-        # functions found within `self.permutations`.
-        iterator = [fn(curr_species) * self.rates[i]
-                    for i, fn in enumerate(self.permutations)]
+        # Calculate the permutations available for each reaction.
+        av_values = [permutation_count(self.species, species_change) * rates
+                        for species_change, rates
+                        in zip(self.species_changes , self.rates)]
 
+        print(av_values)
         # Return a numpy array of floats from the iterator above.
-        return np.fromiter(iterator, np.float)
+        # return np.fromiter(iterator, np.float)
+        return np.fromiter(av_values, np.float)
 
     def calc_tau(self, Av_sum, random_value):
         """Calculate the Tau value, which is the probable length of time
@@ -162,7 +179,7 @@ class Gillespie:
         while self.rxn_count < self.max_sim_rxn:
 
             # Calculate the Av values.
-            Av = np.array(self.calc_av(self.species))
+            Av = np.array(self.calc_av())
 
             # Sum the Av values.
             Av_0 = np.sum(Av)
@@ -177,7 +194,6 @@ class Gillespie:
 
             # Determine mu, the index of which reaction occurs.
             mu = self.calc_mu(Av, Av_0, r_two)
-
             # Increment the reaction counter by one.
             self.rxn_count += 1
 
@@ -238,9 +254,12 @@ class CompleteGillespie(Gillespie):
         # appropriate *args or **kwargs.
         super().__init__(*args, **kwargs)
 
-        # Create the advanced output arays.
+        # Create the advanced output arrays.
         self.av_out = np.empty(shape=(self.max_sim_rxn, len(self.rates)))
         self.mu_out = np.empty(self.max_sim_rxn)
+
+        self.av_out[0, :] = np.NAN
+        self.mu_out[0] = np.NAN
 
     def simulate(self):
         """
@@ -264,12 +283,12 @@ class CompleteGillespie(Gillespie):
             self.time_out[self.rxn_count] = self.time
 
             # Calculate the Av values.
-            Av = np.array(self.calc_av(self.species))
+            Av = np.array(self.calc_av())
             self.av_out[self.rxn_count, :] = Av
             # print("Av", Av)
 
             # Sum the Av values.
-            Av_0 = sum(Av)
+            Av_0 = np.sum(Av)
             # print("Av_0", Av_0)
 
             # Generate two random numbers.
@@ -278,9 +297,14 @@ class CompleteGillespie(Gillespie):
             # Calculate tau, the time until a reaction occurs. Then
             # increment the current time by that value.
             tau = self.calc_tau(Av_0, r_one)
+
+            # If tau reaches infinity, we can stop the simulation.?
+            if tau == np.inf:
+                break
             self.time += tau
 
             # Determine mu, the index of which reaction occurs.
+            # print(Av, Av_0, r_two)
             mu = self.calc_mu(Av, Av_0, r_two)
             self.mu_out[self.rxn_count] = mu
 
@@ -288,16 +312,16 @@ class CompleteGillespie(Gillespie):
             self.rxn_count += 1
 
             # Calculate the new population of species based on the mu index.
-            self.species = self.species_changes[mu](self.species)
+            self.species += self.species_changes[mu]  #(self.species)
 
         # When the loop is over (the maximum number of reactions to be
         # simulated has been reached) return a tuple of the time and
         # species values.
         output_dict = {
-            "time": self.time_out,
-            "species": self.species_out,
-            "av": self.av_out,
-            "mu": self.mu_out
+            "time": self.time_out[:self.rxn_count + 1],
+            "species": self.species_out[:self.rxn_count + 1],
+            "av": self.av_out[:self.rxn_count + 1],
+            "mu": self.mu_out[:self.rxn_count + 1]
         }
         return output_dict
 
